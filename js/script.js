af519789e9f11e0932a6a6be84de035f92f1b9eb
vladimir.polishchuk <vladimir.polishchuk@ringcentral.com>
2023-11-20 16:53:03 +0100

    new practice

--- js/script.js
+++ js/script.js
@@ -1,38 +1,60 @@
 "use strict";
-const numberOfFilms = +prompt ('Сколько фильмов вы уже посмотрели?', '');
 
+let namberOfFilms;
 
-const personalMovieDb = {
-    count: numberOfFilms,
-    movies: {}, 
-    actors: {},
-    genres: [],
+function start(){
+    namberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', ''); //первый промт - сколько фильмов посмотрел 
+
+    while (namberOfFilms == '' || namberOfFilms == null || isNaN(namberOfFilms)){
+        namberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    }
+}
+
+start();
+
+const personalMoovieDB = { //DB
+    count:namberOfFilms,
+    movies:{},
+    actors:{},
+    generes:[],
     privat: false
 };
-  
-    for(let i = 0; i < 2; i++){
-        const a = prompt('Один из последних просмотренных фильмов?', ''),
-              b = prompt('На сколько оцените его?', '');
-
-        if (a != null && b != null && a != '' && b != '' && a.length < 50){
-            personalMovieDb.movies[a] = b;
-        } else{
-            console.log('ERROR')
-            i--;
-        }
-
-        
-    }
-
-    console.log(personalMovieDb);
-
-    if(personalMovieDb.count == 0) {
-        alert('he is not watch moovies');
-    } else if(personalMovieDb.count < 10 && personalMovieDb.count !=0) {
-        alert('Недодрочил до 10-ти фильмов');
-    }else{ 
-        alert('много фильмов смотришь, скоро ослепнешь!');
     
+function rememberMyFilms(){
+    for (let i = 0; i < 2; i++ ) {
+        const a = prompt ('Назовите один из последних просмотренных фильмов', ''),
+              b = +prompt ('оцените этот фильм по шкале от 1-10','');
+        if (a !=null && b != null && a != '' && b != '' && a.length < 50){
+            personalMoovieDB.movies[a] = b;
+        } else {
+            i--;
+        }  
     };
+}
 
+rememberMyFilms();
+
+function detectPersonalLevel(){
+    if(personalMoovieDB.count <10){
+        alert ('Смотришь мало фильмов');
+    }else if (personalMoovieDB.count >=10 && personalMoovieDB.count <=30){
+        alert ('больше деяти и меньше тридцати!');    
+    } else if (personalMoovieDB.count >30){
+        alert('Больше тридцати - дрочер киноман!');
+    } else {
+        alert ('Хуйня какая-то')
+    };
+}
+
+detectPersonalLevel();
+console.log (personalMoovieDB);  
+
+// // function showMyDb (privat){
+// //     return personalMoovieDB;
+// // }
+
+// // let result = privat(a,b)
    
+
+
+
